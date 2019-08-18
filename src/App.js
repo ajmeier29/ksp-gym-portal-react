ad814c0d6e3b2c1f3cf63d50bbc23d136ca70594
Andrew Meier <ajmeier29@gmail.com>
2019-08-17 20:32:48 -0400

    Added eslint-react and configured .eslintrc.json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { allResolved } from 'q';
-import { useState, useEffect } from 'react';
-import { Container, Button, Alert, Fade, Collapse } from 'react-bootstrap';
+// import { allResolved } from 'q';
+import { useState } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Alert,
+  Fade,
+  Collapse,
+  Card
+} from 'react-bootstrap';
 import { CSSTransition } from 'react-transition-group';
 import { setTimeout } from 'timers';
+import PropTypes from 'prop-types';
 
 const testWorkout = {
   _id: {
@@ -76,46 +86,62 @@ const testWorkout = {
   ]
 };
 
-function Example() {
-  const [open, setOpen] = useState(false);
+// const LineItem = (props) => {
+//   return (
+//     <Container>
+//       <Row>
+//         <Col style={{ display: 'inline-block' }}>
+//           <div style={{ display: 'inline-block'}}>
+//             <a href="/#" className="btn btn-primary" >
+//               Show Workout
+//             </a>
+//             <div className="card border-light d-inline-block" >
+//               <div className="card-body">
+//                 This is some text within a card body.
+//             </div>
+//             </div>
+//           </div>
+//         </Col>
+//         <Col><Button>Show Workout</Button></Col>
+//       </Row>
+//     </Container>
+//   )
+// }
 
-  const handleClick = () => {
-    setOpen(true);
-    setTimeout(() => setOpen(false), 3000);
-  };
+const divStyle = {
+  position: 'absolute',
+  top: 5,
+  bottom: 0,
+  left: '10%',
+  right: 0,
+  margin: 'auto'
+};
 
+const LineItem = props => {
   return (
-    <>
-      <Container style={{ paddingTop: '2rem' }}>
-        <Button onClick={handleClick}>Show Toast Message</Button>
+    <div style={divStyle}>
+      <Container bsPrefix="w-100 p-3">
+        <Row>
+          <Col lg="8">
+            <div className="card">
+              <div className="card-body">Workout Information blah bla bla</div>
+            </div>
+          </Col>
+          <Col xs={2}>
+            <div className="card">
+              <div className="card-body btn btn-primary">Show Workout</div>
+            </div>
+          </Col>
+        </Row>
       </Container>
-      <ShowToast show={open} onClickFn={setOpen} />
-    </>
-  );
-}
-
-const ShowToast = props => {
-  return (
-    <Fade in={props.show}>
-      <Container style={{ paddingTop: '2rem' }}>
-        <CSSTransition in={props.show} timeout={1000} classNames="toastAlert">
-          <Alert
-            variant="primary"
-            dismissible
-            onClose={() => props.onClickFn(false)}
-          >
-            <Alert.Heading>Test Toast Message</Alert.Heading>
-          </Alert>
-        </CSSTransition>
-      </Container>
-    </Fade>
+    </div>
   );
 };
 
-const DisplayList = props => {
+const DisplayList = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="card test">
+    <div className="card line-item-card">
       <a href="/#" className="btn btn-primary" onClick={() => setOpen(!open)}>
         Go somewhere
       </a>
@@ -131,10 +157,52 @@ const DisplayList = props => {
 function App() {
   return (
     <div>
-      <ShowToast />
-      <Example />
+      <LineItem />
+      {/* <ShowToast /> */}
+      {/* <Example /> */}
       <DisplayList />
     </div>
   );
 }
 export default App;
+
+// function Example() {
+//   const [open, setOpen] = useState(false);
+
+//   const handleClick = () => {
+//     setOpen(true);
+//     setTimeout(() => setOpen(false), 3000);
+//   };
+
+//   return (
+//     <>
+//       <Container style={{ paddingTop: '2rem' }}>
+//         <Button onClick={handleClick}>Show Toast Message</Button>
+//       </Container>
+//       <ShowToast show={open} onClickFn={setOpen} />
+//     </>
+//   );
+// }
+
+// const ShowToast = props => {
+//   return (
+//     <Fade in={props.show}>
+//       <Container style={{ paddingTop: '2rem' }}>
+//         <CSSTransition in={props.show} timeout={1000} classNames="toastAlert">
+//           <Alert
+//             variant="primary"
+//             dismissible
+//             onClose={() => props.onClickFn(false)}
+//           >
+//             <Alert.Heading>Test Toast Message</Alert.Heading>
+//           </Alert>
+//         </CSSTransition>
+//       </Container>
+//     </Fade>
+//   );
+// }
+
+// ShowToast.propTypes = {
+//   show: PropTypes.string,
+//   onClickFn: PropTypes.func
+// }
