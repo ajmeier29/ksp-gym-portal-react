0c1a325b5d0919e502a0e9968ef0b226fa10e5a6
Andrew Meier <ajmeier29@gmail.com>
2019-09-15 13:43:22 -0400

    Fixed issue where state was not rendering and properly implemented state controls

--- src/create-workout/Create-Workout-Form.js
+++ src/create-workout/Create-Workout-Form.js
@@ -57,6 +57,49 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+// TEMP, NEED TO REMOVE
+// REPLACE WITH WEB API CALL
+const arrLocations = [
+  {
+    id: '243q3r',
+    name: 'Allentown',
+    phone: '6013905742'
+  },
+  {
+    id: 'wfer43',
+    name: 'Bethlehem',
+    phone: '6103905742'
+  }
+];
+
+const arrDevices = [
+  {
+    id: 'xadfw213423',
+    name: 'Adult Device 1 Alt',
+    location: 'Allentown'
+  },
+  {
+    id: 'asdfvv23431',
+    name: 'Adult Device 2 Alt',
+    location: 'Allentown'
+  },
+  {
+    id: 'averv035',
+    name: 'Adult Device 1 Beth',
+    location: 'Bethlehem'
+  },
+  {
+    id: 'er68sdasd',
+    name: 'Adult Device 2 Beth',
+    location: 'Bethlehem'
+  },
+  {
+    id: '34r34q4f3f',
+    name: 'Intro Device Beth',
+    loccation: 'Bethelehem'
+  }
+];
+
 // const WorkoutName = props => {
 //   return (
 //     <>
@@ -74,36 +117,63 @@ const useStyles = makeStyles(theme => ({
 //   );
 // };
 
+// removes an item from an array based of of an 'id' attribute
+const removeFromArrayById = (arr, fnMatch) => {
+  const index = arr.findIndex(fnMatch);
+  return arr.slice(0, index).concat(arr.slice(index + 1, arr.length));
+};
+
 // The entire workout form assembled together
 const CreateWorkoutEntryForm = () => {
   const classes = useStyles();
 
-  const [locations, setLocations] = useState([]);
-  const [devices, setDevices] = useState([]);
+  const [locations] = useState(arrLocations); // Locations in NoSQL database
+  const [devices, setDevices] = useState([]); // Devices in NoSQL database
+  const [formLocations, setFormLocations] = useState([]); // Locations to be submitted
+  let [formDevices, setFormDevices] = useState([]); // Devices to be submitted
+  let [formWorkout, setFormWorkout] = useState([]); // Workout to be submitted
+  let [formSeries, setFormSeries] = useState([]); // Series to be submitted
+  let [formExercises, setFormExercises] = useState([]); // Exercises to be submitted
 
   const [workoutName, setWorkoutName] = useState('Enter Workout Name');
 
   const handleSubmit = event => {
-    event.preventDefault();
-    let test = event.currentTarget;
-    const data = new FormData(event.target);
+    var test = formLocations;
+    // event.preventDefault();
+    // let test = event.currentTarget;
+    // const data = new FormData(event.target);
 
-    console.log(`WorkoutName in handlesubmit: ${workoutName}`);
+    // console.log(`WorkoutName in handlesubmit: ${workoutName}`);
   };
 
   const handleLocationChange = event => {
-    const index = locations.findIndex(x => x.id === event.target.value),
-      tempArray = [...locations];
-    if (index === -1) {
-      // Add new location
-      locations.push;
+    // get the index of the location selected for use in the locations array
+    const index = locations.findIndex(x => x.id === event.target.value); //,
+    //tempArray = [...locations];
+
+    let isChecked = event.target.checked;
+
+    if (isChecked === true) {
+      // Add to locations list
+      let joined = formLocations.concat(locations[index]);
+      setFormLocations(joined);
+    } else {
+      // Remove the location
+      let removed = removeFromArrayById(
+        formLocations,
+        x => x.id == locations[index].id
+      ); //x => x.id == )
+      setFormLocations(removed);
     }
+    // update the devices list
+    setDevices([...arrDevices]);
   };
 
-  const handleWorkoutNameChange = event => {
-    setWorkoutName(event.target.value);
-    console.log(`WorkoutName: ${workoutName}`);
-  };
+  // useEffect(() =>
+  // {
+
+  //   setDevices({arrDevices});
+  // })
 
   return (
     <div className={classes.root}>
@@ -113,6 +183,7 @@ const CreateWorkoutEntryForm = () => {
             <Paper>
               <FilterSelectionComponent
                 locations={locations}
+                locationHandleFn={handleLocationChange}
                 devices={devices}
               />
             </Paper>
@@ -143,91 +214,61 @@ const CreateWorkoutEntryForm = () => {
 const FilterSelectionComponent = props => {
   return (
     <>
-      <LocationSelectionFilter locations={props.locations} />
+      <LocationSelectionFilter
+        locations={props.locations}
+        locationHandleFn={props.locationHandleFn}
+      />
       <DevicesSelectionFilter devices={props.devices} />
     </>
   );
 };
 FilterSelectionComponent.propTypes = {
   locations: PropTypes.array,
-  devices: PropTypes.array
+  devices: PropTypes.array,
+  locationHandleFn: PropTypes.func
 };
 
 // Component that shows the filter for each location
 // The user selects which locaiton they would like, based off of this,
 // the correct devices are shown in the DevicesSelectionFilter
-const LocationSelectionFilter = () => {
+const LocationSelectionFilter = props => {
   const classes = useStyles();
-  const [locations, setLocations] = useState([]);
-
-  // TEMP, NEED TO REMOVE
-  // REPLACE WITH WEB API CALL
-  const arrLocations = [
-    {
-      id: '243q3r',
-      name: 'Allentown',
-      phone: '6013905742'
-    },
-    {
-      id: 'wfer43',
-      name: 'Bethlehem',
-      phone: '6103905742'
-    }
-  ];
+  // const [locations, setLocations] = useState([]);
 
   // useEffect(() => {
-  //   setLocations(arrLocations);
+  //   setLocations(props.locations);
   // }, []);
   return (
     <ExpandingCheckBox
       classes={classes}
-      dataArr={arrLocations}
+      dataArr={props.locations}
       summaryName={'Locations'}
+      handleChange={props.locationHandleFn}
     />
   );
+};
+LocationSelectionFilter.propTypes = {
+  locations: PropTypes.array,
+  locationHandleFn: PropTypes.func
 };
 
 // This component shows the devices to display the workouts on. They are
 // shown based on what is selected in the location filter
 const DevicesSelectionFilter = props => {
   const classes = useStyles();
-  const [devices, setDevices] = useState([]);
-
-  const devicesArray = [
-    {
-      id: 'xadfw213423',
-      name: 'Adult Device 1 Alt',
-      location: 'Allentown'
-    },
-    {
-      id: 'asdfvv23431',
-      name: 'Adult Device 2 Alt',
-      location: 'Allentown'
-    },
-    {
-      id: 'averv035',
-      name: 'Adult Device 1 Beth',
-      location: 'Bethlehem'
-    },
-    {
-      id: 'er68sdasd',
-      name: 'Adult Device 2 Beth',
-      location: 'Bethlehem'
-    },
-    {
-      id: '34r34q4f3f',
-      name: 'Intro Device Beth',
-      loccation: 'Bethelehem'
-    }
-  ];
 
   return (
     <ExpandingCheckBox
       classes={classes}
-      dataArr={devicesArray}
+      dataArr={props.devices}
       summaryName={'Devices'}
+      handleChange={props.handleChange}
     />
   );
+};
+DevicesSelectionFilter.propTypes = {
+  devices: PropTypes.array,
+  handleChange: PropTypes.func
 };
 
 // this function returns an expanding checkbox
@@ -245,7 +286,11 @@ const ExpandingCheckBox = props => {
           </Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className={props.classes.elementForground}>
-          <CheckBoxFormGroup classes={props.classes} dataArr={props.dataArr} />
+          <CheckBoxFormGroup
+            classes={props.classes}
+            dataArr={props.dataArr}
+            handleChange={props.handleChange}
+          />
         </ExpansionPanelDetails>
       </ExpansionPanel>
     </div>
@@ -255,7 +300,8 @@ const ExpandingCheckBox = props => {
 ExpandingCheckBox.propTypes = {
   classes: PropTypes.object,
   dataArr: PropTypes.array,
-  summaryName: PropTypes.string
+  summaryName: PropTypes.string,
+  handleChange: PropTypes.func
 };
 
 const CheckBoxFormGroup = props => {
@@ -274,7 +320,8 @@ const CheckBoxFormGroup = props => {
     });
     // set the initial state
     setCheckBoxDataArr(tempArr);
-  }, []);
+    console.log('called CheckBoxFormGroup');
+  }, [props.dataArr]);
 
   // const handleChange = (id) => {
   //   var data = checkBoxDataArr;
