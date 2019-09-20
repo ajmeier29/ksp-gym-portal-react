43c28836cd8a2ea700b919aecf0017006b37158a
Andrew Meier <ameier@coordinatedhealth.com>
2019-09-20 16:08:22 -0400

    Cleaned up imports

--- src/create-workout/Workout-Side-Filter.js
+++ src/create-workout/Workout-Side-Filter.js
@@ -1,39 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
-import { ThemeProviderwithStyles, withStyles } from '@material-ui/styles';
 import {
-  Paper,
-  Grid,
   ExpansionPanel,
   ExpansionPanelSummary,
   Typography,
   ExpansionPanelDetails,
   FormControl,
-  FormLabel,
-  TextField,
   FormGroup,
   Checkbox,
-  FormControlLabel,
-  FormHelperText
+  FormControlLabel
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@material-ui/icons/CheckBox';
-import {
-  formControlLableCheckBox,
-  formGroupFilterTheme,
-  formWorkoutTextTheme
-} from '../themes/themes.js';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  KeyboardDatePicker,
-  InlineDatePicker
-} from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
 import { useStyles } from './Create-Workout-Form';
 
 // removes an item from an array based of of an 'id' attribute
