f2def442a5ed16448febf6b2922117ec52e2fabc
Andrew Meier <ajmeier29@gmail.com>
2019-10-13 07:56:44 -0400

    Cleaned up workout summary

--- src/create-workout/New-Workout-Summary.js
+++ src/create-workout/New-Workout-Summary.js
@@ -1,34 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from '@material-ui/styles';
-import { Paper, Grid, TextField, Typography, Button } from '@material-ui/core';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-  KeyboardTimePicker
-} from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  FilterSelectionComponent,
-  locationCheckboxProcedures,
-  containsObject,
-  deviceCheckboxProcedures
-} from './Workout-Side-Filter';
-import { useStyles, seriesHeadingTheme, iconTheme } from './styles.js';
-import { ThemeProvider } from '@material-ui/styles';
-import {
-  Exercise,
-  NormalFormTextField,
-  Series,
-  ControlPointAdd,
-  TypographyField
-} from './Workout-Fields.js';
-import ControlPointIcon from '@material-ui/icons/ControlPoint';
-import { getOptions, alertmessage, parseJSON } from '../api/api-calls';
-import { Redirect } from 'react-router';
+import { useStyles } from './styles.js';
 
-// The entire workout form assembled together
+// Displays the workout that was just persisted
+// TODO: Cleanup further and remove ID's and other useless data.
 const NewWorkoutSummary = props => {
   const classes = useStyles();
   const data = props.location.state.posted_date;
@@ -93,47 +68,10 @@ const Summary = props => {
   );
 };
 
-// const PrintObjectInBullets = props =>
-// {
-//     const keys = Object.keys(props.data)
-
-//     for (var key in validation_messages) {
-//         // skip loop if the property is from prototype
-//         if (!validation_messages.hasOwnProperty(key)) continue;
-
-//         var obj = validation_messages[key];
-//         for (var prop in obj) {
-//             // skip loop if the property is from prototype
-//             if (!obj.hasOwnProperty(prop)) continue;
-
-//             // your code
-//             alert(prop + " = " + obj[prop]);
-//         }
-//     }
-//     return (
-//         <>
-//             {keys.map(key =>(
-//                 // skip loop if the property is from prototype
-//                 if (!validation_messages.hasOwnProperty(key)) continue;
-//             ))}
-//         </>
-//     )
-// }
-
-const TraverseObject = (obj, func) => {
-  for (var key in obj) {
-    // skip loop if the property is from prototype
-    if (!obj.hasOwnProperty(key)) continue;
-
-    var innerobj = obj[key];
-    for (var prop in innerobj) {
-      // skip loop if the property is from prototype
-      if (!innerobj.hasOwnProperty(prop)) continue;
-
-      // call func
-      func(prop);
-    }
-  }
+Summary.propTypes = {
+  data: PropTypes.string,
+  useparent: PropTypes.bool,
+  parentname: PropTypes.string
 };
 
 export { NewWorkoutSummary };
